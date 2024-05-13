6341b6345ac34f284b23189a55fcbaa408723f0d
Ritzl Bence <ritzlbencelevente@ktch.hu>
2024-05-13 12:02:17 +0200

    2024-05-13

--- MagyarorszagTerkepe/MainWindow.xaml.cs
+++ MagyarorszagTerkepe/MainWindow.xaml.cs
@@ -53,24 +53,72 @@ namespace MagyarorszagTerkepe
             // MessageBox.Show($"Beolvasott sorok száma: {db}");
         }
 
-        private void Rajzlap_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Terkep(object sender, MouseButtonEventArgs e)
               
         {
+            Rajzolas();
+        }
+
+        private void Vonal_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Show($"({((Line)sender).X1};{((Line)sender).Y1}"); //jó lenne ha minél nagyobb egy település, annál nagyobb legyen a pont.
+            MessageBox.Show($"{((Rectangle)sender).Margin}");
+        }
+
+        private void Atmeretezes(object sender, SizeChangedEventArgs e)
+        {
+            //MessageBox.Show("Lényegében itt kellene lebonyolítani a rajzolást!");
+            Rajzolas();
+
+        }
+
+        void Rajzolas()
+        {
+            // Jobb volna Line helyett Rectangle vagy Ellipse, ami nagyobb kiterjedésű
+            Rajzlap.Children.Clear(); // reszponzívvá vált.
+            //MessageBox.Show("Méret: " + this.ActualWidth + " x " + this.ActualHeight); // kiírja az ablak aktuális méretét.
+            Rajzlap.Width = this.ActualWidth - 20;
+            Rajzlap.Height = this.ActualHeight - 20;
             double xmeret = Rajzlap.Width / 7;
             double ymeret = Rajzlap.Height / 3;
             for (int i = 0; i < x_ek.Length; i++)
             {
                 //MessageBox.Show("Kattintás");
-                Line vonal = new Line();
-                vonal.X1 = 16/x_ek[i] * xmeret*3;
-                vonal.Y1 = 45.7/y_ok[i] * ymeret*5;
+                Rectangle vonal = new Rectangle();
+                vonal.Width = 7;
+                vonal.Height = 7;
+                vonal.Margin = new Thickness((x_ek[i] - 16) * xmeret + 3, (48.7 - y_ok[i]) * ymeret + 3,0,0);
+                // Mivel a Rectangle-nek és Ellipse-nek is van kiterjedése: szélessége, magassága, érdemes a pozícionálást,
+                // amennyiben 7x7-es, ennek a négyszögnek a középpontjába orientálni.
+                // Alaphelyzetben a pozíció a bal felső sarok lenne, de ha ahhoz képest,
+                // mind vízszintesen, mind függőlegesen a +3 eltolást alkalmazzuk a 7x7-es közepéhez jutunk.
+
+                /*vonal.X1 = (x_ek[i] - 16) * xmeret;
+                vonal.Y1 = (48.7 - y_ok[i]) * ymeret; // ezek csinálták meg a pontokkal kijelölt M.o-t.
                 vonal.X2 = vonal.X1 + 1;
-                vonal.Y2 = vonal.Y1 + 1;
-                vonal.Stroke = Brushes.Black;
-                vonal.StrokeThickness = 5;
+                vonal.Y2 = vonal.Y1 + 1; */
+                
+                //vonal.Stroke = Brushes.Black;
+                if (y_ok[i] > 47.5)
+                {
+                    vonal.Stroke = Brushes.Red;
+                    vonal.Fill = Brushes.Red;
+                }
+                else if (y_ok[i] < 46.3)
+                {
+                    vonal.Stroke = Brushes.Green;
+                    vonal.Fill = Brushes.Green;
+                }
+                else
+                {
+                    vonal.Stroke = Brushes.White;
+                    vonal.Fill = Brushes.White;
+                }
+
+                vonal.StrokeThickness = 2;
+                vonal.MouseUp += Vonal_MouseUp;
                 Rajzlap.Children.Add(vonal);
             }
         }
-       
     }
 }
